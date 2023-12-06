1e5e3b133c4182c8921d736779bc316013814e12
PWarwickDev <pwarwick272@gmail.com>
2023-12-06 12:05:34 -0500

    Paint added

--- Main.java
+++ Main.java
@@ -1,16 +1,30 @@
 import javax.swing.*;
-public class Main {
+import java.awt.*;
+
+
+public class Main extends JPanel {
+
+
     public static void main(String[] args) {
-        JFrame f=new JFrame();//creating instance of JFrame
+        JFrame f=new JFrame("Chess");//creating instance of JFrame
 
-        JButton b=new JButton("click");//creating instance of JButton
-        b.setBounds(400,400,100, 40);//x-axis, y-axis, width, height
-
-        f.add(b);//adding button in JFrame
-
+        f.add(new Main());
         f.setSize(800,800);//800 width and 800 height
-        f.setLayout(null);//using no layout managers
         f.setVisible(true);//making the frame visible
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        int w = 800;
+        int l = 800;
+        Graphics2D wSquare = (Graphics2D) g;
+        Graphics2D bSquare = (Graphics2D) g;
+        wSquare.setColor(Color.WHITE);
+        bSquare.setColor(Color.BLACK);
+        wSquare.fillRect(0,0, 100,100);
+
     }
 }
+
 
