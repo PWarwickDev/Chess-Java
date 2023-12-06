0c2c36d557b2ff947e72150326d96b05710a1c4f
PWarwickDev <pwarwick272@gmail.com>
2023-12-06 16:19:09 -0500

    General warnings fixed

--- Board.java
+++ Board.java
@@ -1,12 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+
 
 
 public class Board {
 
-    private JPanel drawingBoard;
-    private JButton button;
+    //private JButton button;
 
     private static final int GAP = 5;
 
@@ -16,7 +15,7 @@ public class Board {
 
         JPanel contentPane = new JPanel();
         contentPane.setLayout (new BorderLayout(GAP, GAP));
-        drawingBoard = new DrawingBoard();
+        JPanel drawingBoard = new DrawingBoard();
         contentPane.add (drawingBoard, BorderLayout.CENTER);
 
 
@@ -28,33 +27,19 @@ public class Board {
     }
 
     public static void main(String[] args) {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                new Board().displayGUI();
-            }
-        };
+        Runnable runnable = () -> new Board().displayGUI();
         EventQueue.invokeLater( runnable );
     }
 }
 
 class DrawingBoard extends JPanel {
-
-    private static final int TOTAL_RECTANGLES = 5;
     private static final int WIDTH = 800;
     private static final int HEIGHT = 800;
-    private static final int X = 50;
-    private static final int Y = 50;
-    private int counter;
-    private int moveXBy;
-    private boolean isActive;
-    private int count;
+
+
 
     public DrawingBoard() {
         setOpaque(true);
-        counter = 0;
-        count = 0;
-        isActive = false;
     }
 
     @Override
@@ -68,15 +53,14 @@ class DrawingBoard extends JPanel {
         boolean colorToggle = false;
         for (int row = 0; row < HEIGHT; row += 100) {
             for (int col = 0; col < WIDTH; col += 100) {
+                //fillrect overhangs by 1
                 if (!colorToggle) {
                     g.setColor(Color.white);
-                    g.fillRect(col, row, col + 99, row + 99); //fillrect overhangs by 1
-                    colorToggle = !colorToggle;
                 } else {
                     g.setColor(Color.black);
-                    g.fillRect(col, row, col + 99, row + 99); //fillrect overhangs by 1
-                    colorToggle = !colorToggle;
                 }
+                g.fillRect(col, row, col + 99, row + 99); //fillrect overhangs by 1
+                colorToggle = !colorToggle;
             }
             colorToggle = !colorToggle;
         }
